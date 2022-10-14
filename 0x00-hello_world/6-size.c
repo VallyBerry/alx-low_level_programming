6abd18ddd046ab8319eee2a16931c57ae1d571f4
vallyberry <andrekellie267@gmail.com>
2022-10-14 06:33:58 -0700

    My second commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int integerType;
+	float floatType;
+	double doubleType;
+	char charType;
+
+	/* 
+	 * size of operator is used to evaluate
+	 * the size of a variable
+	 */
+	printf("size of int: %ld bytes\n",sizeof(integerType));
+
