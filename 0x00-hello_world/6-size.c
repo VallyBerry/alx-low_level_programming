80a2bfdb6ff50aaadc0948248c2631294a70d1be
vallyberry <andrekellie267@gmail.com>
2022-10-14 07:04:02 -0700

    My second commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
-
+/**
+ * main- is the entry point of the program.
+ *
+ * Return:zero if no error, non-zero values if nerrors.
+ */
 int main(void)
 {
-	int integerType;
-	float floatType;
-	double doubleType;
-	char charType;
-
-	/* 
-	 * size of operator is used to evaluate
-	 * the size of a variable
-	 */
-	printf("size of int: %ld bytes\n",sizeof(integerType));
-
+	printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	printf("Size of an int: %ld byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %ld byte(s)\n", sizeof(float));
+	return (0);
+}
