71b24a804904ce596f5e2315a22aee61c59060de
vallyberry <andrekellie267@gmail.com>
2022-10-15 09:34:14 -0700

    7-print tebahpla task

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - to print all lowercase alphabets in reverse
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char letters;
+
+	for (letters = 'z'; letters = 'a'; letters++)
+		putchar(letters);
+
+	putchar('\n');
+
+	return (0);
+}
