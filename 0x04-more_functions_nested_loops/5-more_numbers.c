e5fbfca6e9435d013b2618e14cb77c01b58a80e6
vallyberry <andrekellie267@gmail.com>
2022-10-20 15:01:00 -0700

    My second commit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,20 +1,23 @@
 #include "main.h"
 
 /**
- * more_numbers - Prints 10 times the numbers from 0 to 14.
- *
- * Return: 0
+ * more_numbers - nmbr fct
+ * Bwave ICT file
+ * Return: no return
  */
+
 void more_numbers(void)
 {
-	int a;
 
-	while (a = 0, a <= 14, a++)
-			{
-				a++;
-				_putchar("%d", a);
-			}
-			_putchar("\n");
-
-			return (0);
+int i, j;
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+			_putchar('1');
+			_putchar(j % 10 + '0');
+		}
+			_putchar('\n');
+		}
 }
