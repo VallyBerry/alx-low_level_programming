5d800a0a015f909419733fe6262d59dc13d90489
vallyberry <andrekellie267@gmail.com>
2022-10-20 14:39:24 -0700

    3-print numbers task

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - to priint the numbers fro 0 t0 9.
+ *
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	char a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prints 10 times the numbers from 0 to 14.
+ *
+ * Return: 0
+ */
+void more_numbers(void)
+{
+	int a;
+
+	while (a = 0, a <= 14, a++)
+			{
+				a++;
+				_putchar("%d", a);
+			}
+			_putchar("\n");
+
+			return (0);
+}
