f7d83aa307d604347c92db90dc14d42bf9453766
vallyberry <andrekellie267@gmail.com>
2022-10-24 12:31:48 -0700

    first task

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,17 +4,9 @@
 /**
  * reset_to_98 - updates the value of a pointer  to 98.
  * @n: 1st variable.
- * @*p: 2nd variable.
  * return: void.
  */
 void reset_to_98(int *n)
 {
-	int n;
-	int *p;
-
-	n = 402;
-	p = &n;
-	_putchar('n');
-	reset_to_98(*p);
-	_putchar('n');
+	*n = 98;
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,19 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
