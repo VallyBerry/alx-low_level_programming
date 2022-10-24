93bcf11e6b06a91b0c51f0cd462a4b230efb51e8
vallyberry <andrekellie267@gmail.com>
2022-10-24 14:13:39 -0700

    Fifth task

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string followd by a new line.
+ * @str: character to be printed.
+ * Return: void.
+ */
+
+void _puts(char *str)
+{
+	_putchar(*str);
+}
