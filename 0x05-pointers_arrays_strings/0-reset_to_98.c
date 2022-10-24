2ee6b8096ddd433a13797d2c71b6bca37e156c38
vallyberry <andrekellie267@gmail.com>
2022-10-24 12:38:10 -0700

    First task

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
  * reset_to_98 - updates the value of a pointer  to 98.
- * @n: 1st variable.
+ * @n: int to check.
  * return: void.
  */
+
 void reset_to_98(int *n)
 {
 	*n = 98;
