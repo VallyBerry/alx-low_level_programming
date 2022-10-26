f0f9087ebb901eb6c551bd284c99fed37aae695d
vallyberry <andrekellie267@gmail.com>
2022-10-26 06:41:45 -0700

    Fourth task

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,3 +1,26 @@
 #9nclude "main.h"
 
-
+/**
+ * _strcmp - compares two strings.
+ * @s1: first int.
+ * @s2: second int.
+ * Return: 0 if same, -1 if s1 < s2, 1 if s1 > s2.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	if (*s1 == *s2)
+	{
+		return (0);
+	}
+	else
+		(*s1 < *s2)
+		{
+			return (-1);
+		}
+	else
+		(*s1 > *s2)
+		{
+			return (1);
+		}
+	return(0)
+}
