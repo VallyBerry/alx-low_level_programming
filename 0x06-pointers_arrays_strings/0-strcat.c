ae3a411b8dc20a2ddf60b035f348f91edc899faf
vallyberry <andrekellie267@gmail.com>
2022-10-26 06:01:58 -0700

    first task

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * *_strcat - concatenates two strings.
+ * @dest: 1st character.
+ * @*b: 2nd character.
+ * Return: pointer.
+ */
+char *_strcat(char *dest, char *src)
+{
+	char dest, *b
+
+	dest = _putchar(*dest, *src);
+	b = &dest;
+	_putchar(dest);
+	return (*b);
+}
