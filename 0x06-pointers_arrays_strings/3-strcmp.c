18c97b43bc009c376479ad41306818e18af9f120
vallyberry <andrekellie267@gmail.com>
2022-10-27 10:34:05 -0700

    ALX tasks

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,26 +1,26 @@
 #include "main.h"
-
 /**
- * _strcmp - compares two strings.
- * @s1: first int.
- * @s2: second int.
- * Return: 0 if same, -1 if s1 < s2, 1 if s1 > s2.
+ * _strcmp - compare 2 string
+ * @s1:string
+ * @s2:strmp
+ * Return:int
+ * Bwave/ Bright
  */
 int _strcmp(char *s1, char *s2)
 {
+	while (((*s1 != '\0') && (*s2 != '\0')) && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
+
 	if (*s1 == *s2)
 	{
 		return (0);
 	}
+
 	else
-		(*s1 < *s2)
-		{
-			return (-1);
-		}
-	else
-		(*s1 > *s2)
-		{
-			return (1);
-		}
-	return(0)
+	{
+		return (*s1 - *s2);
+	}
 }
