77e7738fb676215933b62230b4f2704efa57f746
vallyberry <andrekellie267@gmail.com>
2022-10-31 10:20:00 -0700

    Third task

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * *_strchr - locates a character in a string.
+ * 
+ * Return: a pointer to the first occurence of gthe character c.
+ */
+char *_strchr(char *s, char c)
+{
+	if (s[] == 'c')
+	{
+		return (s);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
