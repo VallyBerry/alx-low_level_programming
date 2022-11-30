50644c84fdfb890b8e38c6d6b9c559517de4c4be
vallyberry <andrekellie267@gmail.com>
2022-11-30 11:58:50 -0800

    8-sum listint task

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * sum_listint - sums up all the data in a given list.
+ * @head: given list.
+ * Return: the sum of all data in the list.
+ */
+int sum_listint(listint_t *head)
+{
+	listint_t *a;
+	int sum;
+	unsigned int i;
+
+	if (head == NULL)
+	{
+		return (1);
+	}
+
+	sum = 0;
+	a = head;
+
+	i = 0;
+	while (a != NULL)
+	{
+		sum += a->n;
+		a = a->next;
+		i++;
+	}
+	return (sum);
+		
+}
