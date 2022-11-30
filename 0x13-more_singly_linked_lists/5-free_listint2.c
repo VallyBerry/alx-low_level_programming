405ef4a2ecdf79ffbc5f043bb462f87a8d9d9e9e
vallyberry <andrekellie267@gmail.com>
2022-11-30 08:16:18 -0800

    5-free listint task

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * free_listint2 - frees the memory after a function.
+ * @head: given list.
+ * Return: void.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *a;
+
+	if (head == NULL)
+	{
+		return;
+	}
+	while(head != NULL)
+	{
+		a = (*head)->next;
+		free(*head);
+		*head = a;
+	}
+}
